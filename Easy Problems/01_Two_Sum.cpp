7641ca31588e619c7e3b4baae905142281cc8b74
AdityaSingh <anshul8303858511@gmail.com>
2025-12-14 15:10:20 +0530

    Two Sum solved

---
+++ Easy Problems/01_Two_Sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> a(n);
+        for (int i = 0; i < n; i++){
+            if(a.count(target - nums[i])){
+                return {i, a[target - nums[i]]};
+            }
+            a[nums[i]] = i;
+        }
+        return {};
+    }
+};
