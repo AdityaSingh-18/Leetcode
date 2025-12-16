31ffe2fe246eda8d285eb72d1d18e3ea59083aed
AdityaSingh <anshul8303858511@gmail.com>
2025-12-16 18:38:02 +0530

    Add Remove Duplicates from Sorted Array solution

---
+++ Easy Problems/07_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,10 @@
+// Problem Link https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        int k = nums.size();
+        return k;
+    }
+};
